1bf6c78ef7246344f9344e10347c971704d09a4e
ppNSC <n2679450052@163.com>
2026-08-05 18:29:39 +0800

    [lab]: complete lab09 1. use 'python3 ok -q code_task --local' for code task, contains over_or_under/make_adder/composed/repeat/gcd

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,10 +1,18 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (cond 
+    ((= num1 num2) 0)
+    ((> num1 num2) 1)
+    ((< num1 num2) -1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+  (lambda (inc) (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (if (= n 0)
+      (lambda (x) x)
+      (composed f (repeat f (- n 1)))))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +24,11 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b)
+  (let ((rest (modulo (max a b) (min a b))))
+    (if (zero? rest)
+      (min a b)
+      (gcd (min a b) rest)
+    )
+  )
+)
