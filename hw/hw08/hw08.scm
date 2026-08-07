ac1e69cbc9bc1c7a71ab2d820f7231bd9d93e11c
ppNSC <n2679450052@163.com>
2026-08-07 13:37:29 +0800

    [homework]: complete hw08 1. use 'python3 ok -q question_id -u --local' for understanding of questions, contains ascending/filter/interleave 2. use 'python3 ok -q question_name --local' to test, contains ascending/filter/interleave/no_repeats

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,7 +1,36 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+    (cond
+        ((< (length s) 2) #t)
+        ((> (car s) (car (cdr s))) #f)
+        (else (ascending? (cdr s)))
+    )
+)
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (cond
+        ((null? s) nil)
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))
+    )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (cond
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))
+    )
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+    (if (null? s) nil
+        (cons (car s)
+            (no-repeats
+                (my-filter
+                    (lambda (n) (not (= (car s) n)))
+                    (cdr s)
+                )
+            )
+        )
+    )
+)
